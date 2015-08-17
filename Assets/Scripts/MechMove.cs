8446956ea020add86f74abf42f28df0a1ee3a4cb
roxursox072 <roxursox072@gmail.com>
2015-08-17 00:47:41 -0700

    unity upload
    unity

---
+++ Assets/Scripts/MechMove.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class MechMove : NetworkBehaviour {
+
+private float moveSpeed = 5;
+Vector3 currentMovement;
+CharacterController controller;
+
+	void Start () 
+	{
+		controller = GetComponent<CharacterController>();
+	}
+	
+	void Update () 
+	{
+		move();
+	}
+
+	void move()
+
+	{
+		if (!isLocalPlayer)
+		{
+			return;
+		}
+
+		if (isLocalPlayer) 
+		{
+		controller.Move(currentMovement * Time.deltaTime);
+
+		currentMovement = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, 0, Input.GetAxis("Vertical") *moveSpeed);
+		}
+	}
+}
